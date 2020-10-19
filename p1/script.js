399c757215211b492be7529f232092d30f2dd5a1
gabrielajohnson <gabrielagj23@gmail.com>
2020-10-18 21:52:50 -0400

    new logic and class binding to site

--- p1/script.js
+++ p1/script.js
@@ -1,58 +1,126 @@
-const app = Vue.createApp({
+Vue.component('match-result', {
     data() {
         return {
-            item: '',
-            items: [],
-            choices: ["Rock","Paper","Scissors"],
-            submit: document.getElementById("shoot"),
-            result: document.getElementById("result"),
-            userScore: document.getElementById("user-score"),
-            computerScore: document.getElementById("computer-score"),
-            versus: document.getElementById("versus"),
-            userScoreCount: 0,
-            computerScoreCount: 0
         }
     },
-    methods: {
+    props: ['roundnumber','results','computerthrow','userthrow'],
+    template: '#match-result',
+});
+
+function resetData(){
+  return {
+      choices: ["Rock","Paper","Scissors"],
+      result: false,
+      versus: false,
+      userScoreCount: 0,
+      computerScoreCount: 0,
+      resultsText: '',
+      rounds: [],
+      round: 1,
+      level: 0,
+      lastUserChoice: '',
+      lastUserChoice2: '',
+      lastMatchLost: false,
+      choicesWereMatch: false,
+      matchEnd: false,
+      levelChosen: false
+  }
+}
+
+
+const app = new Vue({
+  el: '#app',
+  data() {
+    return resetData();
+  },
+  computed: {
+   computerScore() {
+      let cScore1 = this.computerScoreCount;
+      return cScore1;
+    }
+  ,
+   userScore() {
+      let uScore1 = this.userScoreCount;
+      return uScore1;
+  }
+  },
+  methods: {
+          start(){
+                        
+            this.level = document.querySelector('input[name="levelChoice"]:checked').value;
+            this.levelChosen = true;
+          },
           play(){
-            this.versus.style.display = "block";
-            /*this.result.style.display = "block";
+            this.versus = true;
+            this.result = true;
 
             let choice = document.querySelector('input[name="choice"]:checked').value;
             if(choice){
-              getResult(choice);   
-            }*/
-            console.log("play");
+              this.getResult(choice);   
+            }
+          },
+          reset(){
+            Object.assign(this.$data, resetData());
           },
           getResult(choice){
-  
-            let computerChoice = choices[Math.floor((Math.random() * 3))];
-
-
+            let computerChoice;
+            if(this.level == 0){
+              computerChoice = this.choices[Math.floor((Math.random() * 3))];   
+            }else if(this.level == 1){
+              computerChoice = this.difficultChoice(choice);
+            }
             if(choice == computerChoice){
-                  this.result.innerHTML = "It's a tie!";
-                  this.versus.innerHTML = choice + " vs " + computerChoice;
+                  this.resultsText = "It's a tie!";
+                  this.matchEnd = "tie";
             }else if( (choice == "Rock" && computerChoice == "Scissors") || (choice == "Paper" && computerChoice == "Rock") || (choice == "Scissors" && computerChoice == "Paper")){
               this.userScoreCount++;   
-              this.userScore.querySelector("span").innerHTML = userScoreCount;
-              this.result.innerHTML = "You won!";
-              this.versus.innerHTML = choice + " vs " + computerChoice;
+              this.resultsText = "You won!";
+              this.matchEnd = true;
+              if(this.choicesWereMatch){
+                this.lastMatchLost = true;
+              }
             }else{
-              computerScoreCount++;
-              computerScore.querySelector("span").innerHTML = computerScoreCount;
-              this.result.innerHTML = "You lose!";
-              this.versus.innerHTML = choice + " vs " + computerChoice;
+              this.computerScoreCount++;
+              this.resultsText = "You lose!";
+              this.matchEnd = false;
+              if(this.choicesWereMatch){
+                this.lastMatchLost = false;
+              }
             }
+            this.rounds.push({
+              roundnumber: this.round++,
+              results: this.resultsText,
+              userthrow: choice,
+              computerthrow: computerChoice
+            })  
 
+        },
+        difficultChoice(choice){
+          let computerChoice;
+          
+          if(this.lastUserChoice == '' || this.lastUserChoice2 == ''){
+            computerChoice = this.choices[Math.floor((Math.random() * 3))];  
+          }
+            
+          
+          if(this.lastUserChoice && (this.lastUserChoice == this.lastUserChoice2) && this.lastMatchLost == false){
+
+            if(this.lastUserChoice == 'Scissors'){
+              computerChoice = 'Paper';
+            }else if(this.lastUserChoice == 'Paper'){
+              computerChoice = 'Rock'; 
+            }else if(this.lastUserChoice == 'Rock'){
+              computerChoice = 'Scissors'; 
+            }  
+            this.choicesWereMatch = true;
+          }else{
+            computerChoice = this.choices[Math.floor((Math.random() * 3))]; 
+            this.choicesWereMatch = false;
+          }
+          
+          this.lastUserChoice2 = this.lastUserChoice;
+          this.lastUserChoice = choice;
+          return computerChoice;
         }
     }
-}).mount('#app');
-
-
-
-
-
-/*submit.addEventListener("click", function(event){
-  event.preventDefault();
-  play();
-});*/
+});
