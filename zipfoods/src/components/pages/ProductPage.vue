fc42c0cf3bc6060e93fa107d6f778322e5de7405
gabrielajohnson <gabrielagj23@gmail.com>
2020-10-28 21:39:37 -0400

    zipfoods

---
+++ zipfoods/src/components/pages/ProductPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Show Product ID# {{ id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    props: ['id'],
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style>
+</style>
